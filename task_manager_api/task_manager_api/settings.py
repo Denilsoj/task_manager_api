739a14aa9c4dcaadfe1a41583d85ec77b9b1742b
Denilson Oliveira <juniod04@gmail.com>
2024-09-08 21:35:41 -0300

    feat:add tasks app

--- task_manager_api/task_manager_api/settings.py
+++ task_manager_api/task_manager_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks.app.TasksConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
