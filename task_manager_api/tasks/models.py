2fec1a9275996f71353596d49c8901e20a1e1e8e
Denilson Oliveira <juniod04@gmail.com>
2024-09-09 09:18:09 -0300

    feat:add model for tasks

--- task_manager_api/tasks/models.py
+++ task_manager_api/tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tasks(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now=True)
+    google_event_id = models.CharField(max_length=255, null=True, blank=True)
+    time = models.TimeField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['title', 'user'], name='unique_title_per_user')
+        ]
+    
+    def __str__(self):
+        return self.title
