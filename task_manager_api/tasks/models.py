0ed578539387a32b8790e4897b2059dd4bfc023e
Denilson Oliveira <juniod04@gmail.com>
2024-09-11 12:36:15 -0300

    change:change field time for time_start

--- task_manager_api/tasks/models.py
+++ task_manager_api/tasks/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Tasks(models.Model):
+class Task(models.Model):
     title = models.CharField(max_length=45)
     description = models.TextField(null=True, blank=True)
     date = models.DateField()
+    time_start = models.TimeField(null=True, blank=True)
+    time_end = models.TimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now=True)
     google_event_id = models.CharField(max_length=255, null=True, blank=True)
-    time = models.TimeField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     
     class Meta:
