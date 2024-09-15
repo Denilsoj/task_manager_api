921c9e36772d6c1dfbb706a783c4ead33ae033b1
Denilson Oliveira <juniod04@gmail.com>
2024-09-15 00:52:23 -0300

    feat:add environment variable for secret_key

--- task_manager_api/task_manager_api/settings.py
+++ task_manager_api/task_manager_api/settings.py
@@ -1,21 +1,17 @@
+import os
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 load_dotenv()
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%x5q$8%**k#3ih9x%lxqo)tz-z8sk6+h0+l$%_drsq^kh=zgwl'
+SECRET_KEY = os.getenv('SECRET_KEY', 'change')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 
 ALLOWED_HOSTS = []
 
